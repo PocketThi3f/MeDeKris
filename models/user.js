30f72b45043eda70ca598eb5d59cf097a7228ffb
de0lan10 <deonte.lang@gmail.com>
2017-01-29 12:47:34 -0800

    deonte created changed name of database to spotfind_db, created 2 tables: User and Post. Tables are associated. Created db folder containing schema file for for easy local db creation

---
+++ models/user.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    authorid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //The object below is for connecting a single to the potential numerous posts that will be housed in the Post table
+    classMethods: {
+      associate: function(models) {
+        User.hasMany(models.Post);
+      }
+    }
+  });
+  return User;
+};
